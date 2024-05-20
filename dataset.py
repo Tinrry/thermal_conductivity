f408886b3b27c59c7ef03cc106103f6f5550251b
hhzheng <zhenghuanhuan@zhejianglab.com>
2024-05-20 15:29:33 +0800

    将energy + phi放在同一个文件data.csv中

--- dataset.py
+++ dataset.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import re
+
 
 FEATURE_SIZE = 18
 
@@ -9,33 +11,28 @@ def read_txt_and_save_to_csv(feature_file, energy_file,  csv_file):
         header_name.append(f'phi_{i}')
     print(f'header_name: {header_name[:]}')
 
-    df_f = pd.read_csv(feature_file, delimiter=' ', dtype=np.float64, header=None)
-
-    df_e = pd.read_csv(energy_file, delimiter=' ', dtype=np.float64) 
-
+    df_f = pd.read_csv(feature_file, delimiter=' ', dtype=np.float128, header=None)
+    df_e = pd.read_csv(energy_file, delimiter=' ', dtype=np.float128, header=None) 
     df = pd.concat([df_e, df_f], axis=1)
-    print(df_f.shape)
-    print(df)
-
-    df.to_csv(csv_file, index=False, header=header_name)
-
-
-import re
+    df.to_csv(csv_file, index=False, header=header_name, sep=' ')
+    return
 
 def convert_space_CRLF2LF(input_file, output_file):
     # CRLF:\r\n为Windows格式txt, LF：\n为linux的换行格式
     with open(input_file, 'r', newline='\r\n') as infile:
         contents = infile.readlines()
+        # 删除多余空格
         contents = [re.sub(' +', ' ', content).strip().split() for content in contents]
-        print(f'contents {contents}')
     with open(output_file, 'w', newline='\n') as outfile:
         for content in contents:
             outfile.write(' '.join(content) + '\n')
 
-# 浮点数精度问题,精度损失
-# 使用示例
-input_file = 'phi_100.txt'  # 原始文件路径
-output_file = 'phi_100_converted.txt'  # 输出文件路径
-convert_space_CRLF2LF(input_file, output_file)
-df = pd.read_csv(output_file, delimiter=' ', dtype=np.float64, header=None)
-print(df)
+
+# main
+for input_file in  ['phi_100.txt', 'y_100.txt']:
+    output_file = input_file.split('.')[0] + '_converted.txt'
+    convert_space_CRLF2LF(input_file, output_file)
+
+read_txt_and_save_to_csv('phi_100_converted.txt', 'y_100_converted.txt', 'data.csv')
+df = pd.read_csv('data.csv', delimiter=' ', dtype=np.float128)
+print(df.describe())
